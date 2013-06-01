d098c4025840c00334d735fd3eee711a7fffbeb0
BiZoNiX <bizonix@me.com>
2013-06-02 01:10:42 +0300

    some changes

--- GrabProxys.php
+++ GrabProxys.php
@@ -1,14 +1,22 @@
 <?php
 include_once __DIR__.'/beaver/beaver.php';
-$pdo = new \PDO('sqlite:ProxyDB.db');
-$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);  // Recommended
+define('FileDB', __DIR__.'/ProxyDB.db');
+
+try {
+    $pdo = new \PDO('sqlite:'.FileDB);
+    $pdo->setAttribute(\PDO::ATTR_PERSISTENT, true);
+    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Подключение не удалось: ' . $e->getMessage();
+}
+
 Beaver\Base::set_database($pdo);
 
 class ProxyDB extends Beaver\Base
 {
     protected static $_table = 'proxy';  // Required
-    protected static $_pk = 'id';        // Optional
-    public $id;
+//    protected static $_pk = 'id';        // Optional
+//    public $id;
     public $ipIN;
     public $port;
     public $type;
@@ -21,21 +29,20 @@ class ProxyDB extends Beaver\Base
     
     function __construct(){
         //FIX it
-        if(filesize('ProxyDB.db')===0)
+        if(filesize(FileDB)===0)
         {
             self::$_db->exec('CREATE TABLE `proxy` (
-                 `id`        INTEGER    NOT NULL PRIMARY KEY AUTOINCREMENT,
+--               `id`        INTEGER    NOT NULL PRIMARY KEY AUTOINCREMENT,
                  `ipIN`      TEXT       NOT NULL,
                  `port`      INTEGER    NOT NULL,
                  `type`      TEXT       NOT NULL,
-                 `anonimity` TEXT,
+                 `level`     TEXT,
                  `ipOUT`     TEXT,
                  `country`   TEXT       NOT NULL DEFAULT \'N/A\',
                  `Cookie`    INTEGER    NOT NULL DEFAULT 0,
                  `Header`    INTEGER    NOT NULL DEFAULT 0,
                  `Get`       INTEGER    NOT NULL DEFAULT 0
-            );
-            ');
+            );');
         }
     }
     
@@ -47,16 +54,16 @@ class ProxyDB extends Beaver\Base
 $obj = new ProxyDB();
 
 foreach(array(
-    'vpngeeksparser'=>"select ip,type,port from `swdata`",
-    'proxynovaparser'=>"select ip,'HTTP',port from `proxynova`",
-    'hide_my_ass_proxy_list_ip'=>"select ipaddress,_type,port from `hidemyass`",
+    'vpngeeksparser'             =>"select ip,type,port from `swdata`",
+    'proxynovaparser'            =>"select ip,'HTTP',port from `proxynova`",
+    'hide_my_ass_proxy_list_ip'  =>"select ipaddress,_type,port from `hidemyass`",
     'hide_my_ass_proxy_list_ip_4'=>"select ipaddress,_type,port from `hidemyass`",
     'hide_my_ass_proxy_list_ip_5'=>"select ipaddress,_type,port from `hidemyass`",
     'hide_my_ass_proxy_list_ip_6'=>"select ipaddress,_type,port from `hidemyass`",
     'hide_my_ass_proxy_list_ip_7'=>"select ipaddress,_type,port from `hidemyass`",
     'hide_my_ass_proxy_list_ip_8'=>"select ipaddress,_type,port from `hidemyass`",
     'hide_my_ass_proxy_list_ip_9'=>"select ipaddress,_type,port from `hidemyass`",
-    'proxyparser1'=>"select ipaddress,_type,port from `hidemyass`",
+    'proxyparser1'               =>"select ipaddress,_type,port from `hidemyass`",
     
 ) as $user=>$querty){
     $count=$limit=0;$step=1000;
@@ -64,15 +71,15 @@ foreach(array(
     do 
     {
         $Content = file_get_contents($uri = "http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsonlist&name=".$user."&query=" . urlencode($querty . " limit $limit,$step"));
-        $jsons = json_decode($Content, 1);
-        $limit+= $step;
+        $jsons   = json_decode($Content, 1);
+        $limit  += $step;
         foreach ($jsons['data'] as $json) 
         {
             if(count($obj->uniq(array($json[0],$json[2])))===0)
             {
                 $obj->_flag_as_unsaved()->save(array(
                     'ipIN'=>$json[0],
-                    'type'=>(strpos(strtolower($json[1]),'http')!==false)?'http':'socks',
+                    'type'=>(strpos(strtolower($json[1]),'http')!==false)?0:1,
                     'port'=>$json[2],
                 ));
                 $count++;
